f0376ce85282e2906b73ee9c683e3e772a900db2
rxnnct <rixennoct@rambler.ru>
2024-12-23 02:18:36 +0300

    Removed service interface

--- bot-app/src/main/java/ru/rxnnct/botapp/service/PlayerService.java
+++ bot-app/src/main/java/ru/rxnnct/botapp/service/PlayerService.java
@@ -1,16 +1,53 @@
 package ru.rxnnct.botapp.service;
 
+import java.util.NoSuchElementException;
 import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.rxnnct.botapp.entity.Player;
+import ru.rxnnct.botapp.repository.PlayerRepository;
 
-public interface PlayerService {
+@Service
+@RequiredArgsConstructor
+public class PlayerService {
 
-    Player createOrUpdatePlayer(String name, Long tgId);
+    private final PlayerRepository playerRepository;
 
-    Optional<Player> findPlayerByTgId(long playerTgId);
+    @Transactional
+    public Player createOrUpdatePlayer(String name, Long tgId) {
+        try {
+            return playerRepository.findByTgId(tgId)
+                .map(player -> {
+                    player.setName(name);
+                    return playerRepository.save(player);
+                })
+                .orElseGet(() -> {
+                    Player newPlayer = new Player(null, name, tgId);
+                    return playerRepository.save(newPlayer);
+                });
+        } catch (DataIntegrityViolationException e) {
+            throw new IllegalArgumentException(
+                "This name is already taken. Please choose another one.");
+        }
+    }
 
-    void updatePlayer(Integer id, String name, Long tgId);
+    public Optional<Player> findPlayerByTgId(long tgId) {
+        return this.playerRepository.findByTgId(tgId);
+    }
 
-    void deletePlayer(Integer id);
+    @Transactional
+    public void updatePlayer(Integer id, String name, Long tgId) {
+        this.playerRepository.findById(id).ifPresentOrElse(player -> {
+            player.setName(name);
+            player.setTgId(tgId);
+        }, () -> {
+            throw new NoSuchElementException("Player with ID " + id + " not found");
+        });
+    }
 
+    public void deletePlayer(Integer id) {
+        this.playerRepository.deleteById(id);
+    }
 }
