27c56f8839a1c0826a27a6df5d3a3881d38a307f
rxnnct <rixennoct@rambler.ru>
2025-03-28 01:53:03 +0300

    PvE activity progress methods added

--- game-app/src/main/java/ru/rxnnct/gameapp/game/service/PveService.java
+++ game-app/src/main/java/ru/rxnnct/gameapp/game/service/PveService.java
@@ -1,6 +1,8 @@
 package ru.rxnnct.gameapp.game.service;
 
+import java.time.Duration;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.rxnnct.gameapp.core.entity.Player;
@@ -14,6 +16,7 @@ public class PveService {
 
     private final GameCharacterService gameCharacterService;
     private final PlayerService playerService;
+    private final StringRedisTemplate redisTemplate;
 
     @Transactional
     public Long exploreDungeon(Long tgId) {
@@ -29,5 +32,19 @@ public class PveService {
         gameCharacterService.addCurrency(playerId, income);
 
         return income;
+    }
+
+    public void setPveActivityInProgress(Long tgId, boolean inProgress, String pveActivityName) {
+        String key = "%s:%d".formatted(pveActivityName, tgId);
+        if (inProgress) {
+            redisTemplate.opsForValue().set(key, "true", Duration.ofMinutes(5));
+        } else {
+            redisTemplate.delete(key);
+        }
+    }
+
+    public boolean isPveActivityInProgress(Long tgId, String pveActivityName) {
+        String key = "%s:%d".formatted(pveActivityName, tgId);
+        return Boolean.TRUE.equals(redisTemplate.hasKey(key));
     }
 }
